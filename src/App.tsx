5da47cac28891c46411cd57c8d748ea9f0662115
Vitaly Bibik <wital2010@gmail.com>
2021-01-02 23:54:11 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavBar} from "./components/Navbar";
+
+
+const App:React.FC = () => {
+  return  <>
+  <NavBar />
+  <div className='container'>
+    <h1>Test</h1>
+  </div>
+  </>
+}
+
+export default App;
